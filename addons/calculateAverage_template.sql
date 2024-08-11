69756cf534a72cbe39cce341361aeab20edb4609
zyzonix <zyzonix@gmail.com>
2024-08-11 19:27:46 +0200

    added mySQL template for calculation addon

---
+++ addons/calculateAverage_template.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 11, 2024 at 05:25 PM
+-- Server version: 10.11.6-MariaDB-0+deb12u1
+-- PHP Version: 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mgfield.calculated`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `calculateAverage_template`
+--
+
+CREATE TABLE `calculateAverage_template` (
+  `time_utc` datetime(3) NOT NULL,
+  `time_local` datetime(3) NOT NULL,
+  `time_utc_start` datetime(3) NOT NULL,
+  `time_utc_end` datetime(3) NOT NULL,
+  `calculated` float NOT NULL,
+  `avg_out_value` float NOT NULL,
+  `count_of_avg` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
